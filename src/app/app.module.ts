d70f699531cbcad83b25ea05e73dc493cc9c577d
MitchSmoot <mitchsmoot@gmail.com>
2022-06-01 02:26:55 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { PortfolioItemComponent } from './components/portfolio-item/portfolio-item.component';
+import { HelpComponent } from './pages/help/help.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    PortfolioComponent,
+    PortfolioItemComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
