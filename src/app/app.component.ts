e06550e8e14258558a37ca32f14cdcea8451f62d
MitchSmoot <mitchsmoot@gmail.com>
2023-04-04 01:43:54 -0600

    removed experimental code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,28 @@
 import { Component } from '@angular/core';
+import * as THREE from 'three';
+
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+
+})
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight)
+camera.position.setZ(30);
+renderer.render(scene, camera);
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100)
+const material = new THREE.MeshBasicMaterial()
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus)
+function animate() {
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+}
+
+animate()
 
 @Component({
   selector: 'app-root',
@@ -8,3 +32,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'mitchs-website-v2';
 }
+ 
