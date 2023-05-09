ccfe3a2c82c415329d2b79cfb85e3bcb9337402c
MitchSmoot <mitchsmoot@gmail.com>
2023-05-09 14:26:19 -0600

    enabled routing between home and portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomeComponent
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent
   }
 
 ];
